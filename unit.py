5a519ca1d3489bf03118a0fe6b1e48fb03f5fe3a
vishakhsahni <vishakhedu@gmail.com>
2017-06-13 15:57:25 -0400

    test

---
+++ unit.py
@@ -0,0 +1,12 @@
+import unittest
+
+class ForVideo(unittest.TestCase):
+    def setUp(self):
+        print 'Hi My name is Vishakh'
+    def test_search_in_python_org(self):
+        print 'I am from Fairfax va'
+    def tearDown(self):
+        print 'I work in Nest Tech'
+
+    if __name__== "__main__":
+        unittest.main()
